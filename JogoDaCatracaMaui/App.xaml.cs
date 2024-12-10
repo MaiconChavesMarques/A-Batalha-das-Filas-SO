60203410881e0e9d7a96b5027fe9c60d9f914380
Rodrigo Lima <guico21@gmail.com>
2024-12-10 17:28:51 -0300

    GUI

---
+++ JogoDaCatracaMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JogoDaCatracaMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JogoDaCatracaMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JogoDaCatracaMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
